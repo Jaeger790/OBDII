b81ce96f4ec28a92c3b63538a0af9b144042173d
Jaeger790 <b.rott8401@gmail.com>
2024-01-26 21:08:02 -0800

    optimize engine commands

--- Engine_Commands.py
+++ Engine_Commands.py
@@ -1,54 +1,37 @@
-
 import obd
 
 class Engine_Commands:
-    #create a constructor
+    #Constructor
     def __init__(self):
-        #create a connection to the car using the obd module
         self.connection = obd.OBD(portstr="COM7")
-        #instantiate the commands class from the obd module
         self.commands = obd.commands
 
-
-        #create a function that will return the fuel level
+        #Get the fuel level
     def get_fuel_level(self):
-        response = self.connection.query(self.commands.FUEL_LEVEL)
-        return response
+        return self.connection.query(self.commands.FUEL_LEVEL)
 
-        #create a function that will return the engine rpm
+        #Get the engine RPM
     def get_engine_rpm(self):
-        response = self.connection.query(self.commands.RPM)
-        return response
+        return self.connection.query(self.commands.RPM)
 
-        #create a function that will return the coolant temperature
+        #Get the coolant temperature
     def get_coolant_temp(self):
-        response = self.connection.query(self.commands.COOLANT_TEMP)
-        return response
+        return self.connection.query(self.commands.COOLANT_TEMP)
 
-        #create a function that will return the speed
+        #Get the vehicle speed
     def get_speed(self):
-        response = self.connection.query(self.commands.SPEED)
-        return response
+        return self.connection.query(self.commands.SPEED)
     
-        
-
-        #create a function that will return the throttle position
+        #Get the throttle position
     def get_throttle_position(self):
-        #return the throttle position
-        response = self.connection.query(self.commands.THROTTLE_POS)
-        return response
+        return self.connection.query(self.commands.THROTTLE_POS)
 
-            
-
-        #create a function that will return the intake air temperature
+        #Get the intake air temperature
     def get_intake_air_temp(self):
-        #return the intake air temperature
-
         return self.connection.query(self.commands.INTAKE_TEMP)
 
-    #create a function that will return the intake manifold pressure
+        #Get the intake manifold pressure
     def get_intake_manifold_pressure(self):
-        #return the intake manifold pressure
         return self.connection.query(self.commands.INTAKE_PRESSURE)
 
 
